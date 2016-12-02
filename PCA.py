225c6237802ddc679414643206ecd8585e89c252
fuxing4203 <fuxing4203@gmail.com>
2016-12-01 23:20:42 -0500

    Add files via upload

--- PCA.py
+++ PCA.py
@@ -6,8 +6,6 @@ class PCA:
         #n is the number of words considered
         self.LoDM = LoDM
         self.n = n
-        self.processedList = [[None] + LoDM]
-        print(self.processData())
 
     def processData(self):
         fileA = Document(self.LoDM[0])
@@ -17,7 +15,7 @@ class PCA:
         self.wordlist = []
         for i in worddict:
             self.wordlist.append(i)
-        print(self.wordlist)
+        self.processedList = [[None] + self.wordlist]
         self.dictList = []
         for item in self.LoDM:
             theFile = Document(item)
@@ -25,26 +23,23 @@ class PCA:
             wordlist = theFile.wordlist
             freqMap = BasicStats.createFreqMap(wordlist)
             self.dictList.append(freqMap)
-        print('finished creating dict')
         self.wordCount = []
         for item in self.dictList:
             count = 0
             for i in item:
                 count += item[i]
             self.wordCount.append(count)
-        print(self.wordCount)
-        for w in self.wordlist:
+        for i in range(len(self.LoDM)):
             listofProb = []
-            for i in range(len(self.dictList)):
+            for w in self.wordlist:
                 if w in self.dictList[i]:
-                    print(item[w])
                     listofProb.append(item[w]/self.wordCount[i])
                 else:
                     listofProb.append(0)
-            result = [w]
+            result = [self.LoDM[i]]
             result.extend(listofProb)
             print(result)
             self.processedList.append(result)
         return self.processedList
 
-a = PCA(['GrimmFairyTales.txt','Ulysses.txt'],10)
+#a = PCA(['GrimmFairyTales.txt','Ulysses.txt'],10)
